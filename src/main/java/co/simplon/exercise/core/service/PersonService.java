8fa105627da63b8c1eaa33c212cad2609284b061
Denis SIMON SONEIRA <dsimonsoneira.ext@orange.com>
2016-10-11 20:57:37 +0200

    Init Step 4

--- src/main/java/co/simplon/exercise/core/service/PersonService.java
+++ src/main/java/co/simplon/exercise/core/service/PersonService.java
@@ -2,15 +2,24 @@ package co.simplon.exercise.core.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import co.simplon.exercise.core.model.Person;
+import co.simplon.exercise.core.repository.PersonRepository;
 
 @Repository
 public class PersonService {
 
-	public List<Person> getPersons() {
-		return null;
+	@Autowired
+	public PersonRepository personRepository;
+
+	public List<Person> getAll() {
+		return personRepository.findAll();
+	}
+
+	public Person add(Person person) {
+		return personRepository.save(person);
 	}
 
 }
--- src/main/java/co/simplon/exercise/web/PersonController.java
+++ src/main/java/co/simplon/exercise/web/PersonController.java
@@ -2,6 +2,7 @@ package co.simplon.exercise.web;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,29 +11,46 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.simplon.exercise.core.model.Person;
+import co.simplon.exercise.core.service.PersonService;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/person")
 public class PersonController {
 
-	/**
-	 * EXERCISE 5 : Changer le get pour aller récuperer les données de la base
-	 * de données en passant par le PersonService et le PersonRepository
-	 */
+	@Autowired
+	private PersonService personService;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody List<Person> get() {
-		return null;
+		return personService.getAll();
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public @ResponseBody Person post(@RequestBody Person person) {
+		return personService.add(person);
 	}
 
 	/**
-	 * EXERCISE 6 : Changer le post pour inserer une personne en base de données
-	 * de la base de données en passant par le PersonService et le
-	 * PersonRepository
+	 * EXERCISE 7 : Changer la base de données pour pointer sur votre MySQL
 	 */
-	@RequestMapping(method = RequestMethod.POST)
-	public @ResponseBody Person post(@RequestBody Person person) {
-		return null;
+
+	/**
+	 * EXERCISE 8 : Modifier le delete pour effacer en base de données à partir
+	 * de l'id du client
+	 */
+	public void delete(Integer id) {
+
+	}
+
+	/**
+	 * EXERCISE 9 : Ajouter une méthode update d'une personne en base.
+	 * 
+	 * ATTENTION : Pour faire un update, on doit aussi renseigner l'id pour
+	 * savoir quelle personne on est en train de mettre à jour
+	 */
+	public void update(Person person) {
+
 	}
 
 }
