909d87f83e1a9b0f1140b4e569476ae2d78f0ca4
fabienoger <fab-oger@live.fr>
2016-10-12 17:08:44 +0200

    Finish CRUD

--- src/main/java/co/simplon/exercise/core/service/PersonService.java
+++ src/main/java/co/simplon/exercise/core/service/PersonService.java
@@ -2,15 +2,41 @@ package co.simplon.exercise.core.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Repository;
 
 import co.simplon.exercise.core.model.Person;
+import co.simplon.exercise.core.repository.PersonRepository;
 
 @Repository
 public class PersonService {
+	@Autowired
+	private PersonRepository personRepository;
 
 	public List<Person> getPersons() {
+		return personRepository.findAll();
+	}
+	
+	public Person addPerson(Person person) {
+		return personRepository.save(person);
+	}
+	
+	public Person getPerson(int id) {
+		return personRepository.findOne(id);
+	}
+	
+	public List<Person> findByName(String name) {
+		Person p = new Person(name, null);
+		return personRepository.findAll(Example.of(p));
+	}
+	
+	public Person updatePerson(Person person) {
+		return personRepository.save(person);
+	}
+	
+	public Person deletePerson(int id) {
+		personRepository.delete(id);
 		return null;
 	}
-
 }
--- src/main/java/co/simplon/exercise/web/PersonController.java
+++ src/main/java/co/simplon/exercise/web/PersonController.java
@@ -2,7 +2,9 @@ package co.simplon.exercise.web;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,19 +12,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.simplon.exercise.core.model.Person;
+import co.simplon.exercise.core.service.PersonService;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/person")
 public class PersonController {
-
+	@Autowired
+	private PersonService personService;
 	/**
 	 * EXERCISE 5 : Changer le get pour aller récuperer les données de la base
 	 * de données en passant par le PersonService et le PersonRepository
 	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody List<Person> get() {
-		return null;
+		return personService.getPersons();
 	}
 
 	/**
@@ -32,7 +36,26 @@ public class PersonController {
 	 */
 	@RequestMapping(method = RequestMethod.POST)
 	public @ResponseBody Person post(@RequestBody Person person) {
-		return null;
+		return personService.addPerson(person);
 	}
-
+	
+	@RequestMapping(path = "/{id}")
+	public @ResponseBody Person getPerson(@PathVariable int id) {
+		return personService.getPerson(id);
+	}
+	
+	@RequestMapping(path = "/name/{name}")
+	public @ResponseBody List<Person> findByName(@PathVariable String name) {
+		return personService.findByName(name);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT) 
+	public @ResponseBody Person put(@RequestBody Person person) {
+		return personService.updatePerson(person);
+	}
+	
+	@RequestMapping(path = "/{id}", method = RequestMethod.DELETE) 
+	public @ResponseBody Person delete(@PathVariable int id) {
+		return personService.deletePerson(id);
+	}
 }
